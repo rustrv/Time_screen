a387c7227c5d63a0d9118f8babca624c52cfbba6
rustrv <rustrv@yandex.ru>
2023-12-26 13:06:35 +0300

    код для пробы контейнерезации

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+from colorama import init, Fore, Style
+
+def main():
+    init(autoreset=True)  # Инициализация colorama с авто-сбросом цвета
+
+    try:
+        while True:
+            # Получение текущего времени
+            current_time = time.strftime("%H:%M:%S", time.localtime())
+
+            # Форматирование строки с увеличенным размером шрифта и красным цветом
+            formatted_time = f"{Fore.RED}{Style.BRIGHT}{current_time}{Style.RESET_ALL}"
+
+            # Вывод текущего времени на экран
+            print(formatted_time, end='\r', flush=True)
+
+            # Ожидание 1 секунды
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass  # Обработка прерывания клавиатуры (например, нажатие Ctrl+C)
+
+    print("\nПрограмма завершена.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
