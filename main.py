811e9df7235125437e2fef6b2da6f96dfc187e39
rustrv <rustrv@yandex.ru>
2023-12-27 20:44:10 +0300

    перенос времени каждый раз на новую строку3

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
 
             # Вывод текущего времени на экран
             print(formatted_time, end='\r', flush=True)
-            print()
+            print('11111')
 
 
             # Ожидание 1 секунды
